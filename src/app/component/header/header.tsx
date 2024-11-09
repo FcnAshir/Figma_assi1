724d903c844d0eba89cb5032918a2322b8b569b1
Syed NoorAlam <160272348+FcnAshir@users.noreply.github.com>
2024-11-09 14:15:04 +0500

    Add files via upload

---
+++ src/app/component/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-[#A29875] p-6'>
+        <h1 className='text-[#FFFFFF] text-4xl font-normal'>Purity</h1>
+    </div>
+  )
+}
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "./component/header/header"
+import Hero from './component/hero/hero'
+
+const page = () => {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default page
